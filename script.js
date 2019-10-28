618d2c1c0c7e6d2bb4b0866701e5786173464dbc
Wenyuan Wu <wenyuan.wu@uzh.ch>
2019-10-28 02:11:34 +0100

    need to fix visualization problem

--- script.js
+++ script.js
@@ -27,10 +27,40 @@ $(function () {
         }
 
         $("#palindrome-message").show();
-    });
 
+
+        $("#container").show();
+        $("#container").empty();
+
+        let word_list = inputWord.toLocaleLowerCase().split('');
+        $.each(word_list, function (index, value) {
+            //TODO
+            if (value === word_list[word_list.length - 1]) {
+                $("#container").append("<span id='c" + index + "'>" + value + "</span>");
+            }
+            else {
+                $("#container").append("<span id='d" + index + "'>" + value + "</span>");
+            }
+        });
+    });
 });
 
 function isPalindrome(word) {
-    return word.toLowerCase() === word.toLowerCase().split('').reverse().join('');
+    let word_low = word.toLowerCase();
+    let word_list = word_low.split('');
+    return word_low === word_list.reverse().join('');
 }
+
+// Another way to check if input is palindrome
+// source: https://www.freecodecamp.org/news/two-ways-to-check-for-palindromes-in-javascript-64fea8191fd7/
+// function isPalindrome(str) {
+//     let re = /[^A-Za-z0-9]/g;
+//     str = str.toLowerCase().replace(re, '');
+//     let len = str.length;
+//     for (let i = 0; i < len/2; i++) {
+//         if (str[i] !== str[len - 1 - i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
